f1175f0d5451507ccbff0367d5f59c4066546a7d
nicolasvienot <nvienot@gmail.com>
2021-12-20 14:22:47 -0500

    Update

--- stress.rb
+++ stress.rb
@@ -23,26 +23,43 @@ require 'set'
     email: Faker::Internet.unique.email,
     address: Faker::Address.full_address,
     phone: Faker::PhoneNumber.unique.cell_phone,
-    lorem: Faker::Lorem.paragraph_by_chars(number: 10000, supplemental: true)
+    lorem: Faker::Lorem.paragraph_by_chars(number: 1000, supplemental: true)
+}
+
+@data2 = {
+    lorem: Faker::Lorem.paragraph_by_chars(number: 10000, supplemental: true),
+    profession: Faker::Job.title,
+    email: Faker::Internet.unique.email,
+    address: Faker::Address.full_address,
+    phone: Faker::PhoneNumber.unique.cell_phone 
 }
 
 def create_dataset(nb_documents = 10, base_id)
     documents = []
     nb_documents.times do |n|
-        documents << { base_id: base_id + n }.merge(@data)
+        documents << { base_id: base_id + n,
+        username: Faker::Esport.player,
+        name: Faker::Name.unique.name,
+        }.merge(@data2)
     end
     return documents
 end
 
-client = MeiliSearch::Client.new('https://ms-710d8ca6bff8-133.saas.meili.dev', '14b5ddf195bd156fbac296569bbcbf71a79228a1')
+client = MeiliSearch::Client.new('https://ms-40e9b27258bc-133.saas.meili.dev', '994073c2a74b45c47403acfefd2ef97d8f6b1467')
 
-index = client.index('gaming')
+index_name = 'lorem'
+index = client.index(index_name)
 
 nb_documents = 500000
-batch_size = 1000
+batch_size = 10000
 
 nb_send = (nb_documents/batch_size).ceil
 base_id = 0
+
+client.index(index_name).update_filterable_attributes([
+  'username',
+  'name'
+])
 
 nb_send.times do |nb|
     documents = create_dataset(batch_size, base_id)
