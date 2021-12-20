891b332682a7847e15d2415785ad5980ff5a6100
nicolasvienot <nvienot@gmail.com>
2021-12-20 13:31:01 -0500

    Improve speed

--- stress.rb
+++ stress.rb
@@ -10,25 +10,26 @@ gemfile do
     gem 'meilisearch'
 end
 
-puts "Insallation done"
+puts "Installation done"
 
 require 'faker'
 require 'meilisearch'
 require 'set'
 
+@data = {
+    username: Faker::Esport.player,
+    name: Faker::Name.unique.name,
+    profession: Faker::Job.title,
+    email: Faker::Internet.unique.email,
+    address: Faker::Address.full_address,
+    phone: Faker::PhoneNumber.unique.cell_phone,
+    lorem: Faker::Lorem.paragraph_by_chars(number: 10000, supplemental: true)
+}
+
 def create_dataset(nb_documents = 10, base_id)
     documents = []
     nb_documents.times do |n|
-        documents << {
-                    base_id: base_id + n,
-                    username: Faker::Esport.player,
-                    name: Faker::Name.unique.name,
-                    profession: Faker::Job.title,
-                    email: Faker::Internet.unique.email,
-                    address: Faker::Address.full_address,
-                    phone: Faker::PhoneNumber.unique.cell_phone
-                    lorem: Faker::Lorem.paragraph_by_chars(number: 10000, supplemental: true)
-                }
+        documents << { base_id: base_id + n }.merge(@data)
     end
     return documents
 end
@@ -37,8 +38,8 @@ client = MeiliSearch::Client.new('https://ms-710d8ca6bff8-133.saas.meili.dev', '
 
 index = client.index('gaming')
 
-nb_documents = 10000
-batch_size = 100
+nb_documents = 500000
+batch_size = 1000
 
 nb_send = (nb_documents/batch_size).ceil
 base_id = 0
@@ -49,11 +50,6 @@ nb_send.times do |nb|
     base_id = base_id + batch_size
     puts "batch #{nb + 1} done"
 end
-
-test = Faker::Lorem.paragraph_by_chars(number: 10000, supplemental: true)
-
-puts test
-
 
 
 
